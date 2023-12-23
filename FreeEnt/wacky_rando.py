4f567d29ab7ad6ad133e6ea4a9d9ee6404d93c1a
Pyre-Echo <m.brandlx@gmail.com>
2023-12-22 19:09:49 -0600

    "First draft of edwardeffect wacky"

--- FreeEnt/wacky_rando.py
+++ FreeEnt/wacky_rando.py
@@ -37,6 +37,7 @@ WACKY_CHALLENGES = {
     'saveusbigchocobo'  : 'Save Us,\nBig Chocobo!',
     'isthisrandomized'  : 'Is This Even\nRandomized?',
     'forwardisback'     : 'Forward is\nthe New Back',
+    'edwardeffect'      : 'The Edward Effect',
 }
 
 WACKY_ROM_ADDRESS = BusAddress(0x268000)
@@ -475,3 +476,6 @@ def apply_worthfighting(env):
 
 def setup_saveusbigchocobo(env):
     env.meta['wacky_starter_kit'] = [( 'Carrot', [5] )]
+
+def apply_edwardeffect(env):
+    env.add_file('scripts/wacky/edwardeffect.f4c')
